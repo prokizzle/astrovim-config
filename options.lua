3768e8935f4a592f20956c1e2762cb3afe8e01d5
Nick Prokesch <nick.prokesch@roosterteeth.com>
2023-08-11 12:53:20 -0500

    Add neotest

--- options.lua
+++ options.lua
@@ -16,6 +16,9 @@ return {
     diagnostics_mode = 3, -- set the visibility of diagnostics in the UI (0=off, 1=only show in status line, 2=virtual text off, 3=all on)
     icons_enabled = true, -- disable icons in the UI (disable if no nerd font is available, requires :PackerSync after changing)
     ui_notifications_enabled = true, -- disable notifications when toggling UI elements
+    shortmess = {
+      o = true,
+    },
   },
 }
 -- If you need more control, you can use the function()...end notation
@@ -24,6 +27,6 @@ return {
 --   local_vim.g.mapleader = " "
 --   local_vim.opt.whichwrap = vim.opt.whichwrap - { 'b', 's' } -- removing option from list
 --   local_vim.opt.shortmess = vim.opt.shortmess + { I = true } -- add to option list
---
+--   --
 --   return local_vim
 -- end
--- plugins/user.lua
+++ plugins/user.lua
@@ -9,4 +9,64 @@ return {
   --     require("lsp_signature").setup()
   --   end,
   -- },
+  {
+    "nvim-neotest/neotest",
+    ft = { "ruby" }, -- Only Ruby is left
+    dependencies = {
+      "olimorris/neotest-rspec",
+      {
+        "folke/neodev.nvim",
+        opts = function(_, opts)
+          opts.library = opts.library or {}
+          if opts.library.plugins ~= true then
+            opts.library.plugins = require("astronvim.utils").list_insert_unique(opts.library.plugins, "neotest")
+          end
+          opts.library.types = true
+        end,
+      },
+    },
+    opts = function()
+      return {
+        adapters = {
+          require "neotest-rspec" {
+            rspec_cmd = function()
+              return vim.tbl_flatten {
+                "docker",
+                "compose",
+                "exec",
+                "-i",
+                "-w",
+                "/app",
+                "-e",
+                "RAILS_ENV=test",
+                "web",
+                "bundle",
+                "exec",
+                "rspec",
+              }
+            end,
+
+            transform_spec_path = function(path)
+              local prefix = require("neotest-rspec").root(path)
+              return string.sub(path, string.len(prefix) + 2, -1)
+            end,
+
+            results_path = "tmp/rspec.output",
+          },
+        },
+      }
+    end,
+    config = function(_, opts)
+      local neotest_ns = vim.api.nvim_create_namespace "neotest"
+      vim.diagnostic.config({
+        virtual_text = {
+          format = function(diagnostic)
+            local message = diagnostic.message:gsub("\n", " "):gsub("\t", " "):gsub("%s+", " "):gsub("^%s+", "")
+            return message
+          end,
+        },
+      }, neotest_ns)
+      require("neotest").setup(opts)
+    end,
+  },
 }
