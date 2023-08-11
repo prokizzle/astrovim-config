67988b43101cf8e38cd1db799bfd2566c0916020
Nick Prokesch <nick.prokesch@roosterteeth.com>
2023-08-11 12:59:55 -0500

    Auto install treesitter plugins

--- plugins/treesitter.lua
+++ plugins/treesitter.lua
@@ -1,6 +1,9 @@
 return {
   "nvim-treesitter/nvim-treesitter",
   opts = {
-    -- ensure_installed = { "lua" },
+    -- Automatically install missing parsers when entering buffer
+    auto_install = true,
+    ensure_installed = { "javascript", "typescript", "ruby", "tsx", "html", "css", "json", "toml", "vim" },
+    incremental_selection = { enable = true },
   },
 }
